cf5aa52e16f7ed62aa50efa1bbdde98c230cd34d
Sanberk <sanberk34@gmail.com>
2023-03-31 23:34:30 +0300

    V2 - Added OS release detect algorithm

--- ServerCreator/ServerCreator/Form1.Designer.cs
+++ ServerCreator/ServerCreator/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -51,6 +52,7 @@
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
+            this.label1.Visible = false;
             // 
             // textBox1
             // 
@@ -58,18 +60,30 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Bağlantı kopar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(372, 137);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "GetLog Proxy";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +94,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ServerCreator/ServerCreator/Form1.cs
+++ ServerCreator/ServerCreator/Form1.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
-using WebSocketSharp;
-using WebSocketSharp.Server;
-using System.Net.Sockets;
-using System.Net;
-using System.CodeDom;
+
 
 namespace ServerCreator
 {
@@ -23,112 +17,221 @@ namespace ServerCreator
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        int check = 0;
+        private Task task;
+        private Process process;
+        static async Task Proxy_Start(string targetIP)
         {
-            var proxy = new TcpProxyServer("10.100.5.120", 5555);
-            //var ws = new WebSocketServer("ws://localhost:15555");
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:15555/");
+            listener.Start();
 
-
-            //  ws.Start();
-            proxy.Start();
-            label1.Text = "Server started";
-            
-        }
-
-
-
-
-        class TcpProxyServer
-        {
-            private readonly string _targetAddress;
-            private readonly int _targetPort;
-
-            public TcpProxyServer(string targetAddress, int targetPort)
+            while (true)
             {
-                
-                _targetAddress = targetAddress;
-                _targetPort = targetPort;
-            }
+                var context = await listener.GetContextAsync();
 
-            public void Start()
-            {
-                var listener = new TcpListener(IPAddress.Loopback, 15555);
-                listener.Start();
-                listener.BeginAcceptTcpClient(OnClientConnect, listener);
-            }
-
-            private void OnClientConnect(IAsyncResult result)
-            {
-                var listener = (TcpListener)result.AsyncState;
-                var client = listener.EndAcceptTcpClient(result);
-                var target = new TcpClient(_targetAddress, _targetPort);
-                listener.BeginAcceptTcpClient(OnClientConnect, listener);
-                var forwarder = new TcpForwarder(client.GetStream(), target.GetStream());
-                forwarder.Start();
-            }
-        }
-        class TcpForwarder
-        {
-            private readonly NetworkStream _incoming;
-            private readonly NetworkStream _outgoing;
-            public TcpForwarder(NetworkStream incoming, NetworkStream outgoing)
-            {
-                _incoming = incoming;
-                _outgoing = outgoing;
-            }
-
-            public void Start()
-            {
-                var incomingBuffer = new byte[4096];
-                _incoming.BeginRead(incomingBuffer, 0, incomingBuffer.Length, OnIncomingData, incomingBuffer);
-                var outgoingBuffer = new byte[4096];
-                _outgoing.BeginRead(outgoingBuffer, 0, outgoingBuffer.Length, OnOutgoingData, outgoingBuffer);
-
-            }
-
-            private void OnIncomingData(IAsyncResult result)
-            {
-                var buffer = (byte[])result.AsyncState;
-
-                if (_incoming.CanRead && _incoming.DataAvailable)
+                if (context.Request.IsWebSocketRequest)
                 {
-
-                    var bytesRead = _incoming.EndRead(result);
-
-                    if (bytesRead > 0)
-                    {
-                        _outgoing.Write(buffer, 0, bytesRead);
-                        _incoming.BeginRead(buffer, 0, buffer.Length, OnIncomingData, buffer);
-                    }
+                    await HandleWebSocketRequest(context, targetIP);
                 }
-                
-            }
-            private void OnOutgoingData(IAsyncResult result)
-            {
-                var buffer = (byte[])result.AsyncState;
-                var bytesRead = _outgoing.EndRead(result);
-                if (_outgoing.CanRead && _outgoing.DataAvailable)
-                {
-
-                    if (bytesRead > 0)
-                    {
-                        _incoming.Write(buffer, 0, bytesRead);
-                        _outgoing.BeginRead(buffer, 0, buffer.Length, OnOutgoingData, buffer);
-                    }
-                }
-                
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+        static async Task HandleWebSocketRequest(HttpListenerContext context, string targetIP)
+        {
+            var webSocketContext = await context.AcceptWebSocketAsync(null);
+            var clientWebSocket = webSocketContext.WebSocket;
+            var targetTcpClient = new TcpClient();
+
+
+
+            try
+            {
+                await targetTcpClient.ConnectAsync(targetIP, 5555);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Timeout on connection with TV, please check TV WiFi " + e.StackTrace);
+            }
+
+
+
+            var clientWebSocketTask = Task.Run(async () =>
+            {
+                var buffer = new ArraySegment<byte>(new byte[4096]);
+                while (clientWebSocket.State == WebSocketState.Open)
+                {
+                    var result = await clientWebSocket.ReceiveAsync(buffer, CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+                        targetTcpClient.Close();
+                        break;
+                    }
+                    targetTcpClient.GetStream().Write(buffer.Array, 0, result.Count);
+                }
+            });
+
+            var targetTcpClientTask = Task.Run(async () =>
+            {
+                var buffer = new byte[4096];
+                while (clientWebSocket.State == WebSocketState.Open)
+                {
+                    int count = await targetTcpClient.GetStream().ReadAsync(buffer, 0, buffer.Length);
+                    await clientWebSocket.SendAsync(new ArraySegment<byte>(buffer, 0, count), WebSocketMessageType.Binary, true, CancellationToken.None);
+                }
+            });
+
+            await Task.WhenAll(clientWebSocketTask, targetTcpClientTask);
+        }
+
+
+
+        static void sendCommand(string IP)
+        {
+            try
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    var process = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "node.exe",
+                            Arguments = "websockify.js 15555 " + " " + IP + ":5555",
+                            RedirectStandardOutput = true,
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                        }
+                    };
+                    process.Start();
+                    Console.WriteLine(process.StandardOutput.ReadToEnd());
+                    process.WaitForExit();
+                });
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+
+
+        }
+
+
+
+
+        private async void button1_Click(object sender, EventArgs e)
         {
             string _IP = "";
-
-            if (textBox1.Text != string.Empty)
+            Version v = Environment.OSVersion.Version;
+            try
             {
-                _IP = textBox1.Text;
+                if (textBox1.Text != string.Empty)
+                {
+                    if (textBox1.TextLength <= 13 && check == 3 || check == 4)
+                    {
+                        if (v.Major == 6 && v.Minor == 1)
+                        {
+                            _IP = textBox1.Text;
+                            label1.Text = "Server start";
+                            label1.Visible = true;
+
+                            sendCommand(_IP);
+
+
+
+                        }
+                        else
+                        {
+                            _IP = textBox1.Text;
+                            label1.Text = "Server start";
+                            label1.Visible = true;
+
+                            await Proxy_Start(_IP);
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("The IP addres should be 9 digit and need to seperate with comma", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Please don't left IP input  ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception k)
+            {
+                MessageBox.Show(k.Message);
+            }
+
+
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //check commas
+            if (e.KeyChar == '.')
+            {
+                check += 1;
+            }
+
+            if (check > 5)
+            {
+                check = 0;
+            }
+
+
+            if ((e.KeyChar < 20) || (e.KeyChar >= '0') && (e.KeyChar <= '9') || (e.KeyChar == '.'))
+                return;
+
+            e.Handled = true;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (process != null)
+                {
+                    process.Kill();
+                    MessageBox.Show("Task ended with no problem");
+
+                }
 
             }
+            catch (Exception k)
+            {
+                MessageBox.Show(k.Message);
+            }
+
+            var nodeRunProcess = Process.GetProcessesByName("node");
+
+
+            foreach (var nodePro in nodeRunProcess)
+            {
+                try
+                {
+                    nodePro.Kill();
+                    label1.Text = "Stopped";
+                    MessageBox.Show("Task ended -- Node");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
         }
     }
 }
